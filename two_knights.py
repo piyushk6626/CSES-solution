888934fd24b8dae72b4b433fc489cac6718f3825
piyushk6626 <kulkarnipiyush462@gmail.com>
2024-01-26 23:55:03 +0530

    trying two_knights

--- two_knights.py
+++ two_knights.py
@@ -1,7 +1,19 @@
-def two_knights(n):
-    ways = (n**4 - 9 * n**2 + 24 * n) // 2
-    return ways
-n=int(input())
-for i in range(1,n+1):
-    result = two_knights(i)
-    print(result)
+n = int(input())
+arr = [0, 6, 28, 96]
+ 
+if n <= 4:
+    for i in range(n):
+        print(arr[i])
+else:
+    for i in range(4):
+        print(arr[i])
+ 
+    for i in range(5, n+1):
+        corner = ((i*i) - 2) * 4
+        ad_corner = ((i*i) - 3) * 8
+        edge = (i*i - 4) * ((i*i) - ((i-1)*(i-1)) - 12)
+        mid_sqr_corner = (i*i - 4) * 4
+        mid_edge = (i*i - 6) * (((i-1)*(i-1)) - ((i-2)*(i-2)) - 4)
+        mid_sqr = (i*i - 8) * ((i-2)*(i-2))
+        ans = (corner + ad_corner + edge + mid_sqr_corner + mid_edge + mid_sqr) // 2
+        print(ans)
